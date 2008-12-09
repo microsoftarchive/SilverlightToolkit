22e824b190ca12462074f9181294df1beaff18ff
shawnoster_cp <SND\shawnoster_cp>
2008-12-09 21:36:40 +0000

    Deleting files since the project layout has changed.

--- Development/Silverlight/Controls/Controls.DataVisualization/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// (c) Copyright Microsoft Corporation.
-// This source is subject to the Microsoft Public License (Ms-PL).
-// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
-// All other rights reserved.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "SWC1001:XmlDocumentationCommentShouldBeSpelledCorrectly", MessageId = "Polyline", Justification = "Matches System.Windows.Shapes.Polyline.")]
