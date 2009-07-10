ed5998faa45be2193f440bedae87549d6d4bad10
shawnoster_cp <SND\shawnoster_cp>
2009-07-10 04:59:39 +0000

    Updated to July 2009 Toolkit source code for Silverlight 2 and Silverlight 3. All new files/directories come directly from the "Source code.zip" and "Sample source code.zip" archives that are installed by the Toolkit MSIs.

--- Development/Silverlight3/Source/System.Windows.Controls.Data.DataForm/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// (c) Copyright Microsoft Corporation.
-// This source is subject to the Microsoft Public License (Ms-PL).
-// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
-// All other rights reserved.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Template", "SWC2000:TemplateShouldDefineTargetType", MessageId = "ControlTemplate<>")]
-[assembly: SuppressMessage("Microsoft.Usage", "CA2208:InstantiateArgumentExceptionsCorrectly", Scope = "member", Target = "System.Windows.Controls.DataPager.#OnPageIndexPropertyChanged(System.Windows.DependencyObject,System.Windows.DependencyPropertyChangedEventArgs)")]
