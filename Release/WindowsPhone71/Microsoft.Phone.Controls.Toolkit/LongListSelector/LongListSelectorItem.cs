d6b5d47ca09cd448d54be85802fcdb385097be1b
jeffwilcox_cp <SND\jeffwilcox_cp>
2011-11-04 15:16:56 +0000

    Adding some missing files that were previously nested public types.

---
+++ Release/WindowsPhone71/Microsoft.Phone.Controls.Toolkit/LongListSelector/LongListSelectorItem.cs
@@ -0,0 +1,43 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.Phone.Controls
+{
+    /// <summary>
+    /// Holds information about an item for use in the LongListSelector.
+    /// </summary>
+    public class LongListSelectorItem
+    {
+        /// <summary>
+        /// Gets or sets the item type.
+        /// </summary>
+        public LongListSelectorItemType ItemType
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the associated group for the item.
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "Assists in debugging.")]
+        public object Group
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the underlying item instance.
+        /// </summary>
+        public object Item
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Release/WindowsPhone71/Microsoft.Phone.Controls.Toolkit/LongListSelector/LongListSelectorItemType.cs
@@ -0,0 +1,43 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+namespace Microsoft.Phone.Controls
+{
+    /// <summary>
+    /// Describes different items.
+    /// </summary>
+    public enum LongListSelectorItemType
+    {
+        /// <summary>
+        /// Indicates an unknown item type.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Represents a standard list item.
+        /// </summary>
+        Item,
+
+        /// <summary>
+        /// Represents a group header.
+        /// </summary>
+        GroupHeader,
+
+        /// <summary>
+        /// Represents a group footer.
+        /// </summary>
+        GroupFooter,
+
+        /// <summary>
+        /// Represents a list header.
+        /// </summary>
+        ListHeader,
+
+        /// <summary>
+        /// Represents a list footer.
+        /// </summary>
+        ListFooter,
+    }
+}
